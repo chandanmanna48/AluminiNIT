99cd1e6125ac8a7bd88bf5060c658b3817a79976
chandanmanna48 <chandanmanna48@gmail.com>
2020-01-07 01:03:46 +0530

    static changes

--- myalumini/settings.py
+++ myalumini/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+#STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = '/home/chandanmanna48/AluminiNIT/static'
 
 MEDIA_URL = '/media/'
