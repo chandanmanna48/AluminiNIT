f3abb272e2762325891902167c19c618e04bbcef
chandanmanna48 <chandanmanna48@gmail.com>
2020-01-07 00:37:07 +0530

    update wsgi

--- myalumini/wsgi.py
+++ myalumini/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+#
+## assuming your django settings file is at '/home/chandanmanna48/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/chandanmanna48/mysite/manage.py'
+path = '/home/chandanmanna48/AluminiNIT/myalumini'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
