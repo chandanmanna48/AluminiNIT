14da825853c2e462e406f93332933d6feecf7727
chandanmanna48 <chandanmanna48@gmail.com>
2020-01-07 12:50:45 +0530

    add path db

--- myalumini/settings.py
+++ myalumini/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'myalumini.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/chandanmanna48/AluminiNIT/db.sqlite3',
     }
 }
 
