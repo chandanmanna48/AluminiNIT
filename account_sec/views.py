0331b0dee949180e5eacba9cd24ff746e1455f95
chandanmanna48 <chandanmanna48@gmail.com>
2020-01-18 09:02:17 +0530

    new

--- account_sec/views.py
+++ account_sec/views.py
@@ -184,7 +184,7 @@ def contact_mail(request):
     message = request.POST['message_sender']
     send_mail(name,message,email_sender,['NitAlumini2020@gmail.com'],fail_silently=False)
     result_msg = "Email Sent Successfully "
-    return render(request,'/',{'result_msg':result_msg})
+    return render(request,'',{'result_msg':result_msg})
 
 
 
