4d19e10f2f564e26cd98849954e9c177aa985e98
chandanmanna48 <chandanmanna48@gmail.com>
2020-01-28 23:20:04 +0530

    commits

--- account_sec/views.py
+++ account_sec/views.py
@@ -343,7 +343,7 @@ def regdno_search_result(request):
 
     return render(request,'search_result.html',{'res':res})
 
-    def name_search_result(request):
+def name_search_result(request):
     name = request.POST['top-search-bar-name']
     li = name.split('')
     res = User.objects.get((first_name = li[0] and last_name=li[1]) or first_name=li[0])
