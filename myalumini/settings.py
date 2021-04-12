5fc6757b83bcff373a0ab5bf14811172685da0a2
Chandan Kumar Manna <45963515+chandanmanna48@users.noreply.github.com>
2021-04-12 10:17:40 +0530

    update settings.py

--- myalumini/settings.py
+++ myalumini/settings.py
@@ -196,8 +196,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT= 587
-EMAIL_HOST_USER = 'NitAlumini2020@gmail.com'
-EMAIL_HOST_PASSWORD='nalanda@2020'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD=''
 
 
 
